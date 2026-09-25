06d36881d8b32def11488bf11408a9f789e46798
ma_anthony <19557925280@139.com>
2026-09-25 13:01:49 +0800

    刚刚搞错了

--- 商品目录.c
+++ 商品目录.c
@@ -16,14 +16,5 @@ typedef struct {
     int   count;                /*定义计数*/
 } GoodsCatalog;                 /* 完成商品目录 */
 
-void   goods_seed(GoodsCatalog* cat);
-int    goods_load(GoodsCatalog* cat, const char* path);
-int    goods_save(const GoodsCatalog* cat, const char* path);
-Goods* goods_find(GoodsCatalog* cat, const char* code);
-int    goods_add(GoodsCatalog* cat, const char* code,
-    const char* name, double price, int stock);
-int    goods_del(GoodsCatalog* cat, const char* code);
-void   goods_print(const GoodsCatalog* cat, int show_stock);
-
 #endif /* POS_H */
        
